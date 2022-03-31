f28163d4241546b9ea36840100d48f3685fc5756
Erind Hoxha <erind_cbh@hotmail.com>
2022-04-01 00:57:49 +1300

    modal added stopPropagation, close modal

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import history from "../history";
 
 const Modal = (props) => {
   return ReactDOM.createPortal(
-    <div className="modal d-block fade show" tabindex="-1" role="dialog">
-      <div className="modal-dialog" role="document">
+    <div
+      onClick={() => history.push("/")}
+      className="modal d-block fade show"
+      tabindex="-1"
+      role="dialog"
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="modal-dialog"
+        role="document"
+      >
         <div className="modal-content">
           <div className="modal-header">
             <h5 className="modal-title">Delete stream</h5>
