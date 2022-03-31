74b269e8d130d82beef46959c424da7c56c5f851
Erind Hoxha <erind_cbh@hotmail.com>
2022-04-01 00:44:46 +1300

    added modal

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -3,9 +3,39 @@ import ReactDOM from "react-dom";
 
 const Modal = (props) => {
   return ReactDOM.createPortal(
-    <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">This is a modal!</div>
-    </div>
+    <div className="modal d-block fade show" tabindex="-1" role="dialog">
+      <div className="modal-dialog" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">Delete stream</h5>
+            <button
+              type="button"
+              className="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="modal-body">
+            <p>Are you sure you want to delete this stream?.</p>
+          </div>
+          <div className="modal-footer">
+            <button type="button" className="btn btn-create">
+              Delete
+            </button>
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-dismiss="modal"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>,
+    document.querySelector("#modal")
   );
 };
 
--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Header from "../Header";
+import Modal from "../Modal";
 
 const StreamDelete = () => {
   return (
     <div>
-      <Header minified={true} />
-      <div>StreamDelete</div>;
+      StreamDelete
+      <Modal />
     </div>
   );
 };
