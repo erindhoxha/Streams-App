20ff77036a183f765a52008b75c53e222f9278d4
Erind Hoxha <erind_cbh@hotmail.com>
2022-04-03 10:58:59 +1200

    added header, paragraph

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -10,11 +10,22 @@ class StreamShow extends React.Component {
     console.log("THESE ARE THE PROPS", this.props);
   }
   render() {
+    if (!this.props.stream) {
+      return (
+        <div>
+          <Header minified="true" />
+          Loading...
+        </div>
+      );
+    }
     return (
       <div>
         <Header minified="true" />
-        StreamShow
-        {this.props.stream ? "Hello" : ""}
+        <div className="container mt-4">
+          <h4>{this.props.stream.title}</h4>
+          <p>{this.props.stream.description}</p>
+          <video></video>
+        </div>
       </div>
     );
   }
