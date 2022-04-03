48d4be42a7fbabfdc2307a4ee40f151453dddd38
Erind Hoxha <erind_cbh@hotmail.com>
2022-04-03 22:05:15 +1200

    added rmtp server

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,84 +1,87 @@
-import React from 'react'
-import { Spinner } from 'reactstrap';
-import Config from "../config"
-import { connect } from 'react-redux'
-import { SignIn, SignOut } from "../actions"
-
+import React from "react";
+import { Spinner } from "reactstrap";
+import Config from "../config";
+import { connect } from "react-redux";
+import { SignIn, SignOut } from "../actions";
 
 class GoogleAuth extends React.Component {
-    state = {
-        name: null,
-        id: null
-    }
-    componentDidMount() {
-        this.gAuthInit();
-    }
+  state = {
+    name: null,
+    id: null,
+  };
+  componentDidMount() {
+    this.gAuthInit();
+  }
 
-    gAuthInit() {
-        window.gapi.load('client:auth2', () => {
-            window.gapi.client.init({
-                clientId: `${Config.googleAuthKey}.apps.googleusercontent.com`,
-                scope: "email"
-            }).then(() => {
-                // Initialize AUTH
-                this.auth = window.gapi.auth2.getAuthInstance();
-                // This.auth.isSignedIn if it changes, it listens and it runs the function in the argument
-                this.auth.isSignedIn.listen(this.onAuthChange);
-                // Run onAuthChange with the value it has to initiate state
-                this.onAuthChange(this.auth.isSignedIn.get());
-
-            })
+  gAuthInit() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client
+        .init({
+          clientId: `${Config.googleAuthKey}.apps.googleusercontent.com`,
+          scope: "email",
         })
-    }
+        .then(() => {
+          // Initialize AUTH
+          this.auth = window.gapi.auth2.getAuthInstance();
+          // This.auth.isSignedIn if it changes, it listens and it runs the function in the argument
+          this.auth.isSignedIn.listen(this.onAuthChange);
+          // Run onAuthChange with the value it has to initiate state
+          this.onAuthChange(this.auth.isSignedIn.get());
+        });
+    });
+  }
 
-    onAuthChange = (isSignedIn) => {
-        if (isSignedIn) {
-            this.props.SignIn(this.auth.currentUser.get().getBasicProfile().getId(), this.auth.currentUser.get().getBasicProfile().getName());
-        } else {
-            this.props.SignOut();
-        }
+  onAuthChange = (isSignedIn) => {
+    if (isSignedIn) {
+      this.props.SignIn(
+        this.auth.currentUser.get().getBasicProfile().getId(),
+        this.auth.currentUser.get().getBasicProfile().getName()
+      );
+    } else {
+      this.props.SignOut();
     }
+  };
 
-    onSignInClick = () => {
-        this.auth.signIn();
-    }
+  onSignInClick = () => {
+    this.auth.signIn();
+  };
 
-    onSignOutClick = () => {
-        this.auth.signOut();
-    }
+  onSignOutClick = () => {
+    this.auth.signOut();
+  };
 
-    renderAuthButton() {
-        if (this.props.isSignedIn) {
-            return (<div className="profile">
-                <button className="btn btn-danger "
-                    onClick={this.onSignOutClick}>Sign out
-                </button>
-            </div>
-            )
-        } else if (this.props.isSignedIn === false) {
-            return (<div className="profile">
-                <button
-                    className="btn btn-danger"
-                    onClick={this.onSignInClick}>
-                    <span>Sign in</span>
-                </button>
-            </div>)
-        }
+  renderAuthButton() {
+    if (this.props.isSignedIn) {
+      return (
+        <div className="profile">
+          <button className="btn btn-danger " onClick={this.onSignOutClick}>
+            Sign out
+          </button>
+        </div>
+      );
+    } else if (this.props.isSignedIn === false) {
+      return (
+        <div className="profile">
+          <button className="btn btn-danger" onClick={this.onSignInClick}>
+            <span>Sign in</span>
+          </button>
+        </div>
+      );
     }
+  }
 
-    render() {
-        return (
-            <div>{this.renderAuthButton()}</div>
-        )
-    }
+  render() {
+    return <div>{this.renderAuthButton()}</div>;
+  }
 }
 
 const mapStateToProps = (state) => {
-    // This will be converted from STATE to PROPS
-    return { isSignedIn: state.auth.isSignedIn, id: state.auth.id, name: state.auth.name }
-}
+  // This will be converted from STATE to PROPS
+  return {
+    isSignedIn: state.auth.isSignedIn,
+    id: state.auth.id,
+    name: state.auth.name,
+  };
+};
 
-export default connect(
-    mapStateToProps,
-    { SignIn, SignOut }
-)(GoogleAuth)
+export default connect(mapStateToProps, { SignIn, SignOut })(GoogleAuth);
--- client/src/index.js
+++ client/src/index.js
@@ -5,10 +5,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import reducers from "./reducers";
-import reduxThunk from "redux-thunk"
+import reduxThunk from "redux-thunk";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)));
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(reduxThunk))
+);
 const rootElement = document.getElementById("root");
 
 // add browser router here? with base name
